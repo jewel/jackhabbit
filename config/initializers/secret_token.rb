73cd1b3e7164fa5035e2c1c12e07ce6629cd34a0
jewel <github@thesteve.org>
2013-06-06 23:37:09 -0600

    Remove secret token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Jackhabbit::Application.config.secret_token = '2ce663a09ab1eb551a04edac9f4bfba52f4b6aec0a6fbceeff36a876bfc943dc1f7ac30588c014bc591462befebbf23986b58b81624134f8e166e6de83f04f40'
